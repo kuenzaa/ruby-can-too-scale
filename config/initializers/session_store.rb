8857d68919c49a2e002b6f6f14726734432c1588
Tim Krajcar <allegro@conmolto.org>
2016-05-11 15:39:51 -0700

    remove Rails

--- config/initializers/session_store.rb
+++
@@ -1,3 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-Rails.application.config.session_store :cookie_store, key: '_ruby-can-too-scale_session'
--- db/seeds.rb
+++
@@ -1,5 +0,0 @@
-Article.delete_all
-
-(1..50).each do |i|
-  Article.create(id: i, title: Faker::Hipster.sentence, body: Faker::Hipster.paragraphs(2))
-end
